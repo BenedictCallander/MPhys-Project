9a13c6de2db26bee3f2859decf83a2db2025cf99
btcallander <79093121+btcallander@users.noreply.github.com>
2022-10-07 15:05:58 +0100

    taken surfacelevel data from apipage

---
+++ datasort.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+pd.DataFrame({
+    "id" : ,
+    "link" : ,
+    "mass_gas" : ,
+    "sfr" : ,
+    "mass_baryonic" : ,
+    "mass_stars" : ,
+    "mass_total" : ,
+    "" : ,
+    "" : ,
+    "" : ,
+    "" : ,
+    "" : ,
+    "" : ,
+    "" : ,
+    "" : ,
+    "" : ,
+    "" : ,
+    "" : ,
+    "" : ,
+    "" : ,
+})
