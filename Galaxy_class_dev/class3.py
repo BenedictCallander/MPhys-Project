6b1f6194898400102dd516d233162490bab9c6a2
btcallander <bc763@bath.ac.uk>
2022-10-18 22:28:52 +0100

    pls dont delete

---
+++ Galaxy_class_dev/class3.py
@@ -0,0 +1,112 @@
+
+import logging # http logging for debugging purpouses
+import time #runtime calculation import numpy as np #data handling 
+import requests #obtain data from API server
+import h5py #binary file manipulation
+import pandas as pd 
+import numpy as np 
+import matplotlib.pyplot as plt 
+import illustris_python as il
+headers = {"api-key":"849c96a5d296f005653a9ff80f8e259e"}
+
+basePath='/x/Physics/AstroPhysics/Shared-New/DATA/IllustrisTNG/TNG100-1/output'
+def get(path, params = None):
+    #utility function for API reading 
+
+    #Make API request - 
+    # Path: url to api page 
+    #Params - misc ; Headers = api key 
+    r = requests.get(path, params=params, headers=headers)
+
+    #HTTP code - raise error if code return is not 200 (success)
+    r.raise_for_status()
+    
+    #detect content type (json or hdf5) - run appropriate download programme
+    
+    if r.headers['content-type'] == 'application/json':
+        return r.json() # parse json responses automatically
+
+    if 'content-disposition' in r.headers:
+        filename = r.headers['content-disposition'].split("filename=")[1]
+        with open(filename, 'wb') as f:
+            f.write(r.content)
+        return filename # return the filename string
+
+    return r
+
+'''
+def geturl(simID, snapID):
+    baseurl = 'http://www.tng-project.org/api/'+str(simID)+'/snapshots/'+str(snapID)
+    return baseurl
+'''
+
+class galaxy:
+    def __init__(self,simID,snapID,subID):
+        self.simID = simID
+        self.snapID = snapID
+        self.subID = subID 
+        basePath='/x/Physics/AstroPhysics/Shared-New/DATA/IllustrisTNG/TNG100-1/output'
+        baseurl = 'http://www.tng-project.org/api/'+str(simID)+'/snapshots/'+str(snapID)
+        
+        hubble =0.7
+        
+        snap_util =get(baseurl) #use api to easily read snapshot information
+        redshift = snap_util['redshift']
+        self.redshift =redshift 
+        
+        scalefac = 1./(1.+redshift)
+        
+        #
+        # Read Subhalo level info 
+        #
+
+        ptNumGas = il.snapshot.partTypeNum('gas') #determine index designation for each particle type
+        ptNumStars = il.snapshot.partTypeNum('stars')
+        #pull all data for specific subhalo 
+        all_fields= il.groupcat.loadSingle(basePath, snapID, subhaloID = subID)
+        self.lMgas  = np.log10( all_fields['SubhaloMassInRadType'][ptNumGas]/hubble ) + 10.
+        self.lMstar = np.log10( all_fields['SubhaloMassInRadType'][ptNumStars]/hubble ) + 10.
+        # Coordinate of particle with minimum binding energy (converted from ckpc/h to kpc)
+        self.centre = all_fields['SubhaloPos']/hubble / (1. + redshift)  # 3-element array [units: proper kpc]
+        # Adopt the 3D half-stellar-mass radius
+        self.Rhalf  = all_fields['SubhaloHalfmassRadType'][ptNumStars]/hubble / (1. + redshift)  
+        # [units: proper kpc] (quantified in 3D)
+        
+        # Load all the relevant particle level info
+        gas = il.snapshot.loadSubhalo(basePath, snapID, subID, 'gas', fields=['Coordinates', 'Masses', 'Velocities', 'StarFormationRate'])
+        # dimensions and units (see https://www.tng-project.org/data/docs/specifications/#parttype0):
+        # Coordinates (N,3) ckpc/h   where ckps stands for co-moving kpc
+        # Masses      (N)   10**10 Msun/h
+        # Velocities  (N,3) km sqrt(scalefac)        # We convert these to pkpc (proper kpc), Msun and km/s, respectively
+        crit_dist = 5 * self.Rhalf #30. # proper kpc
+        hcoldgas  = np.where( (gas['StarFormationRate'] > 0.) & (np.sum((gas['Coordinates']/hubble / (1. + redshift) - self.centre[None,:])**2, axis=1) < crit_dist**2) )[0]
+        self.pgas_coo   = gas['Coordinates'][hcoldgas]/hubble / (1. + redshift)
+        self.pgas_m     = gas['Masses'][hcoldgas] * 10**10 / hubble
+        self.pgas_vel   = (gas['Velocities'][hcoldgas] * np.sqrt(scalefac)) - all_fields['SubhaloVel'][None,:]
+        self.conv_kms2kpcyr = (3.1558 / 3.08568) * 10**(-9)
+        self.pgas_vel   = self.pgas_vel * self.conv_kms2kpcyr    #Convert to kpc/yr
+        self.pgas_sfr   = gas['StarFormationRate'][hcoldgas]
+        
+        # Load all stellar particle data
+        stars = il.snapshot.loadSubhalo(basePath, snapID, subID, 'stars', fields=['Coordinates', 'Masses', 'Velocities'])
+        hstar = np.where( (np.sum((stars['Coordinates']/hubble / (1. + redshift) - self.centre[None,:])**2, axis=1) < crit_dist**2) )[0]
+        self.pstar_coo   = stars['Coordinates'][hstar]/hubble / (1. + redshift)
+        self.pstar_m     = stars['Masses'][hstar] * 10**10 / hubble
+        self.pstar_vel   = (stars['Velocities'][hstar] * np.sqrt(scalefac)) - all_fields['SubhaloVel'][None,:]
+        self.pstar_vel   = self.pstar_vel * self.conv_kms2kpcyr
+        
+    def galcen(self):
+        self.pgas_coo -= self.centre[None,:]
+        self.pstar_coo -= self.centre[None,:]
+    
+    
+    def ang_mom_align(self, type):
+        if (type=='gas'):
+            _coo = np.copy(self.pgas_coo)
+            _vel = np.copy(self.pgas_vel)
+            _m = np.copy(self.pgas_m)
+        elif(type=='stars'):
+            _coo =np.copy(self.pstar_coo)
+            _vel = np.copy(self.pstar_vel)
+            _m = np.copy(self.pstar_m)
+            
