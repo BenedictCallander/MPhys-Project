fe0d4a8f79bacecaee9385654a6f593d1bfca347
btcallander <bc763@bath.ac.uk>
2022-10-21 15:48:45 +0100

    metallicity plots complete

--- class_il/classrad.py
+++ class_il/classrad.py
@@ -1,5 +1,6 @@
 
-import logging # http logging for debugging purpouses
+import logging
+from random import random # http logging for debugging purpouses
 import time #runtime calculation import numpy as np #data handling 
 import requests #obtain data from API server
 import h5py #binary file manipulation
@@ -76,8 +77,8 @@ class galaxy:
         # Velocities  (N,3) km sqrt(scalefac)        # We convert these to pkpc (proper kpc), Msun and km/s, respectively
         crit_dist = 5 * self.Rhalf #30. # proper kpc
         self.crit_dist = crit_dist
-        hcoldgas  = np.where( (gas['StarFormationRate'] > 0.) & (np.sum((gas['Coordinates']/hubble / (1. + redshift) - self.centre[None,:])**2, axis=1) < crit_dist**2) )[0]
-        #hcoldgas  = (np.sum((gas['Coordinates']/hubble / (1. + redshift) - self.centre[None,:])**2, axis=1) < crit_dist**2)
+        #hcoldgas  = np.where( (gas['StarFormationRate'] > 0.) & (np.sum((gas['Coordinates']/hubble / (1. + redshift) - self.centre[None,:])**2, axis=1) < crit_dist**2) )[0]
+        hcoldgas  = (np.sum((gas['Coordinates']/hubble / (1. + redshift) - self.centre[None,:])**2, axis=1) < crit_dist**2)
         self.pgas_coo   = gas['Coordinates'][hcoldgas]/hubble / (1. + redshift)
         self.pgas_m     = gas['Masses'][hcoldgas] * 10**10 / hubble
         self.pgas_vel   = (gas['Velocities'][hcoldgas] * np.sqrt(scalefac)) - all_fields['SubhaloVel'][None,:]
@@ -87,12 +88,13 @@ class galaxy:
         self.pgas_met   =gas['GFM_Metallicity'][hcoldgas]
         
         # Load all stellar particle data
-        stars = il.snapshot.loadSubhalo(basePath, snapID, subID, 'stars', fields=['Coordinates', 'Masses', 'Velocities'])
+        stars = il.snapshot.loadSubhalo(basePath, snapID, subID, 'stars', fields=['Coordinates', 'Masses', 'Velocities','GFM_Metallicity' ])
         hstar = np.where( (np.sum((stars['Coordinates']/hubble / (1. + redshift) - self.centre[None,:])**2, axis=1) < crit_dist**2) )[0]
         self.pstar_coo   = stars['Coordinates'][hstar]/hubble / (1. + redshift)
         self.pstar_m     = stars['Masses'][hstar] * 10**10 / hubble
         self.pstar_vel   = (stars['Velocities'][hstar] * np.sqrt(scalefac)) - all_fields['SubhaloVel'][None,:]
         self.pstar_vel   = self.pstar_vel * self.conv_kms2kpcyr
+        self.pstar_met = stars['GFM_Metallicity'][hstar]
         
     def galcen(self):
         self.pgas_coo -= self.centre[None,:]
@@ -148,13 +150,16 @@ class galaxy:
     def radial_coo(self):
         self.pgas_rad_len   = np.sqrt(self.pgas_coo[:,0]**2+self.pgas_coo[:,1]**2)
         self.pstar_rad_len  = np.sqrt(self.pstar_coo[:,0]**2+self.pstar_coo[:,1]**2)
-    def xyzprop_df(self):
-        df=pd.DataFrame({"x": self.pgas_coo[:,0], "y":self.pgas_coo[:,1],"z":self.pgas_coo[:,2], "rad":self.pgas_rad_len,"m": self.pgas_m})
+    def xyzprop_df(self,type):
+        if (type=='met'):
+            df=pd.DataFrame({"x": self.pstar_coo[:,0], "y":self.pstar_coo[:,1],"z":self.pstar_coo[:,2], "rad":self.pstar_rad_len,"m": 10e9*self.pstar_met})
+        elif (type=='mass'):
+            df=pd.DataFrame({"x": self.pstar_coo[:,0], "y":self.pstar_coo[:,1],"z":self.pstar_coo[:,2], "rad":self.pstar_rad_len,"m": self.pstar_m})
         
         self.df = df
         return df
     def gas_plot(self,annuli_pc):
-            #self.ang_mom_align('gas')
+        
         annul1= annuli_pc*self.crit_dist
         df_valid = self.df[self.df['rad']<annul1]
         df_valid =df_valid.round()
@@ -162,7 +167,7 @@ class galaxy:
         plt.figure(figsize=(21,15))
         plt.style.use('dark_background')
             #plt.hist2d(data2['x'],data2['y'], weights=data2['m'],bins=[500,500],cmap = 'inferno',vmin=(min(data2['m'])),vmax = max(data2['m']))
-        plt.scatter(-df_valid['x'],-df_valid['y'],c=np.log10(df_valid['m']),cmap='inferno',vmin=1.01*(min(np.log10(df_valid['m']))), vmax=0.99*(max(np.log10(df_valid['m']))))
+        plt.scatter(-df_valid['x'],-df_valid['y'],c=(12+np.log10(df_valid['m'])),cmap='inferno')# vmin=min(df_valid['m']), vmax = max(df_valid['m']))
             #plt.plot(critical_radius,zeros,'g+', markersize=10,label='critical radius')
         plt.xlabel('$\Delta x$ [kpc/h]')
         plt.ylabel('$\Delta y$ [kpc/h]')
@@ -174,7 +179,7 @@ class galaxy:
         filename = 'temppng/TNG100_sub_{}.png'.format(sub1.subID)
         plt.savefig(filename)
         plt.close()
-    def met_grad(self,annuli_pc):
+    def met_grad_gas(self,annuli_pc):
         df=pd.DataFrame({"x": self.pgas_coo[:,0], "y":self.pgas_coo[:,1],"z":self.pgas_coo[:,2], "rad":self.pgas_rad_len,"metallicity": self.pgas_met})
         annul1= annuli_pc*self.crit_dist
         met_df = df[df['rad']<annul1]
@@ -187,8 +192,19 @@ class galaxy:
         plt.savefig(filename)
         plt.close()
         return print('.')
-            
-            
+    def met_grad_star(self,annuli_pc):
+        df =pd.DataFrame({"rad": self.pstar_rad_len,"met": self.pstar_met })
+        df= df.sample(frac=0.01,random_state=1)
+        annul1= annuli_pc*self.crit_dist
+        met_df = df[df['rad']<annul1]
+        plt.figure(figsize=(21,15))
+        plt.scatter(met_df['rad'], (met_df['met']), 'b.')
+        plt.xlabel('Radial Position')
+        plt.ylabel('metallicity')
+        filename = 'temppng/TNG100_metgrad__stellar_forsub_{}.png'.format(sub1.subID)
+        plt.savefig(filename)
+        plt.close()
+        return print('.')
             
 '''
 massive_url = "http://www.tng-project.org/api/TNG100-1/snapshots/70/subhalos/?order_by=-mass&sfr_gt=0.0/"
@@ -204,13 +220,18 @@ massive_ids = [0, 7516, 21013, 15129, 31129, 39628, 47416, 26558, 44002, 34668, 
 #print(len(sub1.pgas_coo))
 #print(len(sub1.pgas_met))
 
-for i in massive_ids:
-    sub1= galaxy('TNG100-1',70,i)
-    sub1.galcen()
-    sub1.ang_mom_align('gas')
-    sub1.radial_coo()
-    sub1.xyzprop_df()
-    sub1.met_grad(0.25)
+#for i in massive_ids:
+sub1= galaxy('TNG100-1',70,0)
+sub1.galcen()
+sub1.ang_mom_align('stars')
+sub1.radial_coo()
+sub1.xyzprop_df('met')
+sub1.gas_plot(0.75)
+print(sub1.pgas_met)
+
+print(min(sub1.pstar_coo[:,0]))
+print(max(sub1.pstar_coo[:,0]))
+print(len(sub1.pstar_rad_len))
 end = time.time()
 print('runtime = {} seconds'.format(end-start))
 
