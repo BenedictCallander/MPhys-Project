f5fb156ab7d736774c02303dc645239ba45bb5ac
btcallander <bc763@bath.ac.uk>
2022-12-07 13:42:06 +0000

    u

---
+++ evolution/tree_cutouts_download.py
@@ -0,0 +1,47 @@
+# Metallicityev.py 
+# \-> script containing Classes subsequent functions to study the Metallicity evolution of a subhalo's metallicity gradient through the IllustrisTNG snapshots 
+# Created:17/11/2022 
+# Author: Benedict Callander 
+# Respository https://github.com/btcallander/MPhys-Project (private)
+#
+
+#Plotting, numerical functions and dataset manipulation
+import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
+import numpy as np
+import pandas as pd
+#import pwlf
+
+#hdf5 binary file manipulation
+import h5py
+
+#Read data from web API and monitor HTTP traffic 
+import requests  
+
+#specialised functions to query Illustris TNG data 
+import illustris_python as il
+
+#Own module containing utility functions 
+
+#runtime calculation 
+import time
+
+#Computational functions - simultaneous calculations to make use of multi-core CPU
+from joblib import Parallel, delayed
+
+#specific functions for fitting utilities
+from scipy.optimize import curve_fit
+from scipy.signal import medfilt, savgol_filter
+headers = {"api-key":"849c96a5d296f005653a9ff80f8e259e"}
+start =time.time()
+
+
+#
+#input parameters
+#
+'''
+.hdf5 cutouts for target progenitors to each subhalo
+
+
+'''
+
